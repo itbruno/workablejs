9d7ac2104200baac07da61e296b9bb1b82484a78
Bruno Rodrigues <brunowebr@gmail.com>
2019-11-13 23:44:33 -0300

    Update hero and some common styles

--- src/components/partials/HeaderNav.js
+++ src/components/partials/HeaderNav.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
 import logo from '../../assets/images/logo.svg';
+import logoLight from '../../assets/images/logo_light.svg';
 
 class HeaderNav extends Component {
     render() {
+        const type = this.props.type;
         return (
-            <div className="header-nav">
+            <div className={type + ' header-nav'}>
                 <a href="/" className="header-logo">
-                    <img src={logo} className="img-responsive" alt="MyWork logo"/>
+                    <img src={type === 'light' ? logoLight : logo} className="img-responsive" alt="MyWork logo"/>
                 </a>
+
+                <nav>
+                    <a href="/">about</a>
+                </nav>
             </div>
         )
     }
--- src/pages/jobs/index.js
+++ src/pages/jobs/index.js
@@ -1,8 +1,12 @@
 import React, { Component } from 'react';
+
 import List from '../../components/jobs/list';
 import JobsFilter from "../../components/jobs/filter";
 import api from '../../services/api';
 import HeaderNav from '../../components/partials/HeaderNav';
+
+import heroImage from '../../assets/images/hero-image.png';
+
 class Jobs extends Component {
     state = {
         companyData: {}
@@ -23,19 +27,25 @@ class Jobs extends Component {
             <>
                 <header id="header" className="page-home">
                     <div className="container">
-                        <HeaderNav />
-                        
+                        <HeaderNav type="light"/>
+                    </div>
 
-                        <h1>Work at <strong className="block">MyCompany</strong></h1>
+                    <div className="container">
+                        
+                        <h1 className="hero-title">Work at <strong className="block">MyCompany</strong></h1>
                         <p>
                             Lorem ipsum dolor sit amet consectetur adipisicing elit praesentium similique beatae.
                         </p>
 
-                        <div className="header-filter">
-                            <JobsFilter />
-                        </div>
+                        <a href="#jobsFilter" className="mouse hidden-sm"></a>
+
+                        <img src={heroImage} alt="resume illustration" className="hero-image"/>
                     </div>
                 </header>
+
+                <div className="container">
+                    <JobsFilter />
+                </div>
 
                 <List />
             </>
